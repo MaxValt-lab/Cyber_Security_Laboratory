1fc042c848b46576d2f0c05edd7efc156cc04435
Олег Ж. <skrusich2000@gmail.com>
2025-09-14 23:29:50 +1000

    Update main.py

--- servise/main.py
+++ servise/main.py
@@ -1,1 +1,10 @@
+from fastapi import FastAPI
+from router import router
 
+app = FastAPI(title="Cyber Security Laboratory API")
+
+app.include_router(router)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
