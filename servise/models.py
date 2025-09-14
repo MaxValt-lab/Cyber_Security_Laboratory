1e115d7172a936efb6bcb2bed895fe13fbf37352
Олег Ж. <skrusich2000@gmail.com>
2025-09-14 23:42:47 +1000

    Update models.py

--- servise/models.py
+++ servise/models.py
@@ -1,7 +1,14 @@
-from pydantic import BaseModel
+# servise/models.py
+
+from pydantic import BaseModel, Field
 
 class Event(BaseModel):
-    type: str
-    source: str
-    severity: str
-    message: str
+    type: str = Field(..., description="Тип события, напр. system_event")
+    source: str = Field(..., description="Источник события, напр. internal/external")
+    severity: str = Field(..., description="Уровень важности: low/medium/high/critical")
+    message: str = Field(..., description="Описание события")
+
+class EventResult(BaseModel):
+    status: str
+    risk_score: int
+    action: str
